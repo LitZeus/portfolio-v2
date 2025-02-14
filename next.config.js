72b0702a46d10e50810a4454ee0f8c1d1f118f01
tejasathalye <me.tejasathalye@gmail.com>
2025-02-14 14:38:11 +0530

    Fixed image local path acc. to assetPrefix

--- next.config.js
+++ next.config.js
@@ -4,10 +4,6 @@ const nextConfig = {
   basePath: '/portfolio-v2', // Replace with your repository name
   assetPrefix: '/portfolio-v2/', // Ensure this matches the basePath
   images: {
-    remotePatterns: [
-      { protocol: "https", hostname: "res.cloudinary.com", pathname: "**" },
-      { protocol: "https", hostname: "images.unsplash.com", pathname: "**" },
-    ],
     unoptimized: true,
   },
 };
