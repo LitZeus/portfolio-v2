5daf1b34957347097490099c421a8dc9781dc585
tejasathalye <me.tejasathalye@gmail.com>
2025-03-28 01:53:40 +0530

    Resolved ESlint errors

--- app/types/index.ts
+++ app/types/index.ts
@@ -1,13 +1,18 @@
-import { IconType } from "react-icons";
+import { FC } from "react";
 
 export * from "./contactDataType";
 export * from "./highlightsType";
 export * from "./iconT";
 export * from "./project";
 
+interface IconProps {
+  color?: string;
+  size?: number;
+}
+
 export type iconType = {
   name: string;
-  icon: IconType;
+  icon: FC<IconProps>;
   color: string;
 };
 
--- app/utils/technologyIconList.tsx
+++ app/utils/technologyIconList.tsx
@@ -1,128 +1,80 @@
 import {
-  ExpressSVG,
-  HerouiSVG,
-  JavascriptSVG,
-  MongoDBSVG,
-  PrimeReactSVG,
-  PrimeVueSVG,
-  QuasarSVG,
-  ReactSVG,
-  ShadcnSVG,
-  TailwindSVG,
-  TypescriptSVG,
-  VueSVG
+    ExpressSVG,
+    HerouiSVG,
+    JavascriptSVG,
+    MongoDBSVG,
+    PrimeReactSVG,
+    PrimeVueSVG,
+    QuasarSVG,
+    ReactSVG,
+    ShadcnSVG,
+    TailwindSVG,
+    TypescriptSVG,
+    VueSVG
 } from "../components/ui/icons";
 import { iconType } from "../types";
 
-let color: string = "#9ca3af";
+const defaultColor = "#9ca3af";
+
 export const techonologyIconList: iconType[] = [
   {
     name: "JavaScript",
-    icon: (
-      <JavascriptSVG
-        color={color}
-        size={35}
-      />
-    ),
+    icon: JavascriptSVG,
+    color: defaultColor
   },
   {
     name: "TypeScript",
-    icon: (
-      <TypescriptSVG
-        color={color}
-        size={35}
-      />
-    ),
+    icon: TypescriptSVG,
+    color: defaultColor
   },
   {
     name: "Vue",
-    icon: (
-      <VueSVG
-        color={color}
-        size={35}
-      />
-    ),
+    icon: VueSVG,
+    color: defaultColor
   },
   {
     name: "React",
-    icon: (
-      <ReactSVG
-        color={color}
-        size={35}
-      />
-    ),
+    icon: ReactSVG,
+    color: defaultColor
   },
-
   {
     name: "Quasar",
-    icon: (
-      <QuasarSVG
-        color={color}
-        size={35}
-      />
-    ),
+    icon: QuasarSVG,
+    color: defaultColor
   },
   {
     name: "Tailwind",
-    icon: (
-      <TailwindSVG
-        color={color}
-        size={35}
-      />
-    ),
+    icon: TailwindSVG,
+    color: defaultColor
   },
   {
     name: "PrimeVue",
-    icon: (
-      <PrimeVueSVG
-        color={color}
-        size={35}
-      />
-    ),
+    icon: PrimeVueSVG,
+    color: defaultColor
   },
   {
     name: "PrimeReact",
-    icon: (
-      <PrimeReactSVG
-        color={color}
-        size={35}
-      />
-    ),
+    icon: PrimeReactSVG,
+    color: defaultColor
   },
   {
     name: "heroui",
-    icon: (
-      <HerouiSVG
-        color={color}
-        size={35}
-      />
-    ),
+    icon: HerouiSVG,
+    color: defaultColor
   },
   {
     name: "Shadcn",
-    icon: (
-      <ShadcnSVG
-        color={color}
-        size={35}
-      />
-    ),
+    icon: ShadcnSVG,
+    color: defaultColor
   },
   {
     name: "Express",
-    icon: (
-      <ExpressSVG
-        color={color}
-        size={35}
-      />
-    ),
+    icon: ExpressSVG,
+    color: defaultColor
   },
   {
     name: "MongoDB",
-    icon: (
-      <MongoDBSVG
-        color={color}
-        size={35}
-      />
-    ),
-  },
+    icon: MongoDBSVG,
+    color: defaultColor
+  }
 ];
