81aa2424ad72b69e169b2965e2118c47fc3a7731
tejasathalye <me.tejasathalye@gmail.com>
2025-03-28 18:05:31 +0530

    fixing pages

--- next.config.js
+++ next.config.js
@@ -2,13 +2,12 @@
 const nextConfig = {
   images: {
     domains: ['files.catbox.moe', 'images.unsplash.com'],
-    unoptimized: process.env.NODE_ENV === 'production',
+    unoptimized: true,
   },
-  ...(process.env.NODE_ENV === 'production' ? {
-    output: 'export',
-    basePath: '/portfolio-v2',
-    assetPrefix: '/portfolio-v2/',
-  } : {}),
+  output: 'export',
+  basePath: '/tejas-athalye-portfolio',
+  assetPrefix: '/tejas-athalye-portfolio/',
+  trailingSlash: true,
 };
 
 module.exports = nextConfig;
