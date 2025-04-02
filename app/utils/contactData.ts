464be786eab08197a3b1116f41de54218c22fca7
tejasathalye <me.tejasathalye@gmail.com>
2025-04-02 22:24:32 +0530

    Form changes

--- app/utils/contactData.ts
+++ app/utils/contactData.ts
@@ -1,7 +1,7 @@
 import { contactDataType } from "../types";
 
 export const contactData: contactDataType = {
-  email: "me.tejasathalye@gmail.com.com",
+  email: "me.tejasathalye@gmail.com",
   github: "https://github.com/LitZeus",
   linkedIn: "https://linkedin.com/in/tejasathalye",
   x: "https://x.com/artist_tejas",
